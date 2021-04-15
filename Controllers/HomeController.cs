65dbb520747ea19dbcc1ec3cd0ca7ffc5c984b81
grantvt2020 <grantvt@gmail.com>
2021-04-16 08:54:37 +1000

    Add IStoreRepository to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
         {
-            return View();
+            repository = repo;
         }
+
+        public IActionResult Index() => View(repository.Products);
     }
 }
